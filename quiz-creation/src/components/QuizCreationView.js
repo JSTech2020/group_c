69ed13a0d1a5dd5d36666821ff53ce85cfa8c48f
Jakob Erzar <jakob.erzar@gmail.com>
2020-04-06 17:24:51 +0200

    Initial setup for quiz creation - duplicated and cleared from quiz.

---
+++ quiz-creation/src/components/QuizCreationView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./QuizCreationView.css";
+
+function QuizCreationView() {
+  return (
+    <div className="quiz-creation-view">
+      Test
+    </div>
+  );
+}
+
+
+
+
+export default QuizCreationView;
